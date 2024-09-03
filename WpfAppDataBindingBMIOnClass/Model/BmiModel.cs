c8abc91f7c7ca55b47d018a1ae075847f826d992
Frank <fha@easv.dk>
2024-09-03 10:57:49 +0200

    Add project files.

---
+++ WpfAppDataBindingBMIOnClass/Model/BmiModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDataBindingBMIOnClass.Model
+{
+    public class BmiModel : INotifyPropertyChanged
+    {
+		private int height;
+
+		public int Height
+		{
+			get { return height; }
+			set { height = value; 
+				this.Changed();
+                CalcResult();
+            }
+		}
+
+        private int weight;
+
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value;
+                this.Changed();
+				CalcResult();
+            }
+        }
+
+        private void CalcResult()
+        {
+            this.Result = weight / height * height;
+        }
+
+        private int result;
+
+		public int Result
+		{
+			get { return result; }
+			set { result = value;
+                this.Changed();
+            }
+		}
+
+		private void Changed([CallerMemberName] String prop = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+			
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
